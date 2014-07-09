896ed0b438066bd03d61fd04d01c0bf75bb47fff
Gareth Williams <gareth@garethw.net>
2014-07-09 12:07:43 +0100

    Camera class, character class and tile getting

---
+++ Assets/Gerde.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gerde : MonoBehaviour
+{
+
+    public tk2dTileMap Map;
+
+    public int TileId;
+
+    public 
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TileId = Map.GetTileIdAtPosition(new Vector3(transform.position.x,transform.position.y,0f), 17);
+	}
+}
--- Assets/MainCamera.cs
+++ Assets/MainCamera.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainCamera : MonoBehaviour {
+public class MainCamera : MonoBehaviour
+{
+    public GameObject Target;
+    public Vector3 Offset;
+    public float LerpSpeed;
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(transform.position.x, transform.position.y, -200f + Mathf.Sin(Time.realtimeSinceStartup)*100f);
+        //transform.position = new Vector3(transform.position.x, transform.position.y, -200f + Mathf.Sin(Time.realtimeSinceStartup)*100f);
+	    transform.position = Vector3.Lerp(transform.position, Target.transform.position + Offset, LerpSpeed * Time.deltaTime);
 	}
 }
